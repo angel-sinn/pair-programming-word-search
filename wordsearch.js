79ea3d914f93c08622b2763a6417c960726f4c1f
Angel Sinn <angelkysinn@gmail.com>
2021-02-11 01:29:23 +0000

    Pair programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programming with: Danny
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (word.length === 1) return false;
+  if (word === "") return false;
+
+  const wordUpperCase = word.toUpperCase();
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(wordUpperCase)) return true;
+  }
+
+  for (let l of verticalJoin) {
+    if (l.includes(wordUpperCase)) return true;
+  }
+
+  return false;
+};
+
+const transpose = function (matrix) {
+  const finalArr = [];
+
+  for (let j = 0; j < matrix[0].length; j++) {
+    let val = [];
+    for (let a = 0; a < matrix.length; a++) {
+      val.push(matrix[a][j]);
+    }
+    finalArr.push(val);
+  }
+  return finalArr;
+};
+
+module.exports = wordSearch;
